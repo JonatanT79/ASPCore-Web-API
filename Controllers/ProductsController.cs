1a4baf4c2dd21da776af5e1bc369c80ecd727c32
Jonathan Tran <jonathan.t79@hotmail.com>
2020-04-23 03:29:39 +0200

    Reading from Json - File

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,27 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CloudNine.Praktik.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
         // GET: api/products
+        [Route("api/[controller]")]
         [HttpGet]
         public async Task<IEnumerable<string>> GetAsync(int? page, int? pageSize)
         {
             // TODO: Returnera alla produkter, ta hänsyn till pagineringsparametrar om sådana skickats in.
-            return new string[] { "value1", "value2" };
+
+            var webClient = new WebClient();
+            var Json = webClient.DownloadString(@"C:\Users\jonat\OneDrive\Dokument\Cloudnine\Cloudnine-Codetest\Data\products.json");
+            var convert = JsonConvert.SerializeObject(Json);
+
+            // var h = convert.ToString().Where(e => e == 'j').ToString();
+
+            return new string[] { convert };
         }
 
         // GET: api/products/5
-
-        // GET: api/productColors
-
+   
     }
 }
+// snygga till utskriften av json listan
+// använda where sats för att få fram specifierad id?
